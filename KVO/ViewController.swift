f0055c1e6c8ac1c1052536607d9be34ffa2f4f9c
Marius Horga <mhorga@gmail.com>
2015-09-13 00:59:38 +0300

    Initial Commit

---
+++ KVO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KVO
+//
+//  Created by Marius Horga on 9/13/15.
+//  Copyright (c) 2015 Marius Horga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
